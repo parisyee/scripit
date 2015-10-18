b831594aa92dd28a4de3fe9eda9f9778c8595cba
Paris Yee <paris.yee@gmail.com>
2015-10-17 20:16:42 -0400

    Set up Circle CI & require basic auth on production

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby "2.2.3"
 
 gem "rails", "4.2.4"
 
+gem "envied"
 gem "jquery-rails"
 gem "pg"
 gem "sass-rails", "~> 5.0"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate if Rails.env.production?
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |user, password|
+      user == ENVied.BASIC_AUTH_USER &&
+        password== ENVied.BASIC_AUTH_PASSWORD
+    end
+  end
 end
